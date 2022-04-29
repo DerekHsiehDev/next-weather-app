19c9748ac6ee4b60f35013094eed78a105857ab3
derekhsiehdev <devderekhsieh@gmail.com>
2022-04-29 11:23:12 -0700

    env

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,9 +1,18 @@
 import React from "react";
+import cities from "../../lib/city.list.json"
 
 // need to run this if data changes often
 export async function getServerSideProps(context) {
-  const city = getCityID(context.params.city);
+  const city = getCity(context.params.city);
   // api call here
+  console.log(city);
+
+  if(!city) {
+    return {
+      notFound: true,
+    }
+  }
+
   const slug = context.params.city;
 
   return {
@@ -13,10 +22,24 @@ export async function getServerSideProps(context) {
   };
 }
 
-const getCityID = (param) => {
+const getCity = (param) => {
   const cityParam = param.trim();
   const splitCity = cityParam.split("-");
-  console.log(splitCity);
+  const id = splitCity[splitCity.length - 1];
+  console.log(id);
+
+  if(!id) {
+    return null;
+  }
+
+  const city = cities.find(city => city.id.toString() == id);
+
+  if(city) {
+    return city
+  } else {
+    return null
+  }
+
 };
 
 export default function City({ slug }) {
