e5d0e587be67897f6772494abbb4570c4e489273
derekhsiehdev <devderekhsieh@gmail.com>
2022-04-29 15:20:03 -0700

    api call

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -13,11 +13,29 @@ export async function getServerSideProps(context) {
     }
   }
 
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&units=imperial&exclude=minutely`)
+  
+  const data = await res.json()
+
+  if(!data) {
+    return {
+      notFound: true
+    } 
+  } else {
+    console.log(data)
+  }
+
+  console.log(process.env.API_KEY)
+
   const slug = context.params.city;
 
   return {
     props: {
       slug: slug,
+      data: data,
+      city: city,
+      currentWeather: data.current,
+      dailyWeather: data.daily
     },
   };
 }
@@ -42,7 +60,27 @@ const getCity = (param) => {
 
 };
 
-export default function City({ slug }) {
+const getHourlyWeather = (hourlyData) => {
+  const current = new Date()
+
+  current.setHours(curent.getHours(), 0, 0, 0);
+
+  const tomororw = new Date(current);
+  tomororw.setDate(tomororw.getDate() + 1)
+  tomorrow.setHours(0,0,0,0);
+
+  const currentTimeStamp = Math.floor(current.getTime() / 1000)
+  const tomororwTimeStamp = Math.floor(tomororw.getTime / 1000)
+
+
+  const todayData = hourlyData.filter(data => data.dt < tomororwTimeStamp)
+  
+  return todayData;
+
+}
+
+export default function City({ slug, data }) {
+  // console.log(data)
   return (
     <div>
       <h1>city page</h1>
